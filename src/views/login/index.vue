f90bd37e42c709ecde2a9438b563892cd19f4caa
strawberry <semon88@126.com>
2022-05-07 00:32:20 +0800

    ci

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,6 +2,6 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 <template>
-  <div>这是登陆页面</div>
+  <div>这是登陆页面 ci测试</div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
